872554d8788dd13b4d160e2e9a4abab2cd3073e3
Samet Öz <ozsamet544@gmail.com>
2023-12-23 15:09:22 +0300

    model ve modelMapper'lar eklendi

---
+++ Model/OrderDetailModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class OrderDetailModel : IModel
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
